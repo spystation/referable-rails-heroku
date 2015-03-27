4c512d57c0042a3e81aa7971f32d0e5eff803e9c
spystation <charles@spystation.com>
2015-03-27 19:20:58 -0400

    Heroku Add SMTP Support

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,15 +78,33 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # For mailer configs 
-
-  config.action_mailer.delivery_method = :sendmail
+  # ****** LOCAL ONLY! ********
+  #config.action_mailer.delivery_method = :sendmail
   # Defaults to:
   # config.action_mailer.sendmail_settings = {
   #   location: '/usr/sbin/sendmail',
   #   arguments: '-i -t'
   # }
+  #config.action_mailer.perform_deliveries = true
+  #config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default_options = {from: 'no-reply@example.com'}
+  # ***************************
+  
+  # Heroku SMTP Setup
+  config.action_mailer.raise_delivery_errors = true
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'polar-depths-6110.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.default_options = {from: 'no-reply@example.com'}
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'polar-depths-6110.herokuapp.com',
+    :user_name            => ENV["GMAIL_USERNAME"],
+    :password             => ENV["GMAIL_PASSWORD"],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
   
 end
