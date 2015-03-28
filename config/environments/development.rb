1691c9e30424809980d1949fe89ec85f231bc1b3
spystation <charles@spystation.com>
2015-03-28 01:36:11 -0400

    Heroku Send Grid Teaks

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # config.action_mailer.default_url_options = { :host => 'referable.herokuapp.com' }
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = false
   # config.action_mailer.default :charset => "utf-8"
   # config.action_mailer.smtp_settings = {
   #  :address              => "smtp.gmail.com",
@@ -68,13 +68,18 @@ Rails.application.configure do
   #  :enable_starttls_auto => true  }
 
   # Heroku SendGrid Setup
+  config.action_mailer.default_url_options = { :host => 'referable.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-    :address   => "smtp.sendgrid.net",
-    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+    :address              => "smtp.sendgrid.net",
+    :port                 => 587, # ports 587 and 2525 are also supported with STARTTLS
     :enable_starttls_auto => true, # detects and uses STARTTLS
-    :user_name => ENV["SENDGRID_USERNAME"],
-    :password  => ENV["SENDGRID_PASSWORD"], # SMTP password is any valid API key
-    :authentication => 'login',
-    :domain => 'referable.com', # your domain to identify your server when connecting
+    :user_name            => ENV["SENDGRID_USERNAME"],
+    :password             => ENV["SENDGRID_PASSWORD"], # SMTP password is any valid API key
+    :authentication       => 'login',
+    :domain               => 'referable.com', # your domain to identify your server when connecting
 
 end
