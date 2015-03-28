e6949b1e5ba3bce828aef6a0b415703078e64f53
spystation <charles@spystation.com>
2015-03-27 22:10:09 -0400

    Heroku Update App Name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   # Heroku SMTP Setup
   config.action_mailer.raise_delivery_errors = true
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'polar-depths-6110.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'referable.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
@@ -101,7 +101,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => 'polar-depths-6110.herokuapp.com',
+    :domain               => 'referable.herokuapp.com',
     :user_name            => ENV["GMAIL_USERNAME"],
     :password             => ENV["GMAIL_PASSWORD"],
     :authentication       => 'plain',
