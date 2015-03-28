7ce6e6495c757a6b350b8e5e9ea5ed0f7c9bc656
spystation <charles@spystation.com>
2015-03-28 04:34:18 -0400

    Heroku Add SendReg

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,19 +67,5 @@ Rails.application.configure do
   #  :authentication       => 'plain',
   #  :enable_starttls_auto => true  }
 
-  # Heroku SendGrid Setup
-  config.action_mailer.default_url_options = { :host => 'referable.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.sendgrid.net",
-    :port                 => 587, # ports 587 and 2525 are also supported with STARTTLS
-    :enable_starttls_auto => true, # detects and uses STARTTLS
-    :user_name            => ENV["SENDGRID_USERNAME"],
-    :password             => ENV["SENDGRID_PASSWORD"], # SMTP password is any valid API key
-    :authentication       => 'login',
-    :domain               => 'referable.com', # your domain to identify your server when connecting
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,20 +91,36 @@ Rails.application.configure do
   # ***************************
   
   # Heroku SMTP Setup
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = true
   # ActionMailer Config
+  # config.action_mailer.default_url_options = { :host => 'referable.herokuapp.com' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.default :charset => "utf-8"
+  # config.action_mailer.smtp_settings = {
+  #  :address              => "smtp.gmail.com",
+  #  :port                 => 587,
+  #  :domain               => 'referable.herokuapp.com',
+  #  :user_name            => ENV["GMAIL_USERNAME"],
+  #  :password             => ENV["GMAIL_PASSWORD"],
+  #  :authentication       => 'plain',
+  #  :enable_starttls_auto => true  }
+
+  # Heroku SendGrid Setup
   config.action_mailer.default_url_options = { :host => 'referable.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => 'referable.herokuapp.com',
-    :user_name            => ENV["GMAIL_USERNAME"],
-    :password             => ENV["GMAIL_PASSWORD"],
-    :authentication       => 'plain',
-    :enable_starttls_auto => true  }
+    :address              => "smtp.sendgrid.net",
+    :port                 => 587, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name            => ENV["SENDGRID_USERNAME"],
+    :password             => ENV["SENDGRID_PASSWORD"], # SMTP password is any valid API key
+    :authentication       => 'login',
+    :domain               => 'heroku.com', # your domain to identify your server when connecting
+
   
 end
