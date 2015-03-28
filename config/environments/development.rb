2cab6d79b1d8df39ef5452dcc95e4bf77e7fba93
spystation <charles@spystation.com>
2015-03-27 22:52:33 -0400

    Heroku Mod ENV

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,15 +41,31 @@ Rails.application.configure do
 
   # For mailer configs 
 
-  config.action_mailer.delivery_method = :sendmail
+   #config.action_mailer.delivery_method = :sendmail
   # Defaults to:
   # config.action_mailer.sendmail_settings = {
   #   location: '/usr/sbin/sendmail',
   #   arguments: '-i -t'
   # }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+   #config.action_mailer.perform_deliveries = true
+   #config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.default_options = {from: 'no-reply@example.com'}
 
+  # Heroku SMTP Setup
+  config.action_mailer.raise_delivery_errors = true
+  # ActionMailer Config
+  # config.action_mailer.default_url_options = { :host => 'referable.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'gmail.com', #'referable.herokuapp.com',
+    :user_name            =>  ENV["GMAIL_USERNAME"],
+    :password             =>  ENV["GMAIL_PASSWORD"],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 
 end
